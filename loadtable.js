15f3c9fd3c7ec98b8a1828a2d26eb68df7d60334
faltastic <youssef.faltas@gmail.com>
2021-10-16 14:16:39 +0200

    map: switch to Papaparse loader

--- loadtable.js
+++ loadtable.js
@@ -1,57 +1,59 @@
-var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1GxeT-u0qHCKC6knmYKuZed7NXTb6pI0xW_4cfkUz-MY/pubhtml';
-// https://docs.google.com/spreadsheets/d/e/2PACX-1vQpitgwgSdSCdNZt1vHbBKizbucDmnlOcHxOySlprnGh0192kTPh7UStHuNOjHBw-SxWoG1sangl4DG/pubhtml?gid=0&single=true
-//1GxeT-u0qHCKC6knmYKuZed7NXTb6pI0xW_4cfkUz-MY
+const SHEET_CSV =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQpitgwgSdSCdNZt1vHbBKizbucDmnlOcHxOySlprnGh0192kTPh7UStHuNOjHBw-SxWoG1sangl4DG/pub?output=csv";
 
+// const publicSpreadsheetUrl =
+//   "https://docs.google.com/spreadsheets/d/1GxeT-u0qHCKC6knmYKuZed7NXTb6pI0xW_4cfkUz-MY/pubhtml";
 
 function loadTable() {
-    Tabletop.init( 
-        { key: publicSpreadsheetUrl,
-        callback: parseAndMap,
-        simpleSheet: true } 
-        )
+  Papa.parse(SHEET_CSV, {
+    download: true,
+    header: true,
+    complete: function (results) {
+      var data = results.data;
+      // console.log(data);
+      parseAndMap(data);
+    },
+  });
 }
 
-function parseAndMap(dataRaw, tabletop) {
-    //console.log(dataRaw);
+function parseAndMap(rawData) {
+  let data = {
+    type: "FeatureCollection",
+    features: [],
+  };
 
-    let data ={
-        "type": "FeatureCollection",
-        "features": []
+  rawData.forEach((element) => {
+    let dataPoint = {
+      type: "Feature",
+      properties: {},
+      geometry: {
+        type: "Point",
+        coordinates: [],
+      },
     };
 
-    dataRaw.forEach(element => { 
+    dataPoint.properties = {
+      id: parseInt(element.id),
+      Photo: element.Photo,
+      Tag: element.Tag,
+      Title: element.Title,
+      Description: element.Description,
+    };
+    dataPoint.geometry.coordinates = element["Location (Longitude, Latitude)"]
+      .split(", ")
+      .reverse()
+      .map((x) => parseFloat(x));
+    data.features.push(dataPoint);
+  });
 
-        let dataPoint = {
-            "type": "Feature",
-            "properties": {},
-            "geometry": {
-              "type": "Point",
-              "coordinates": []
-            }
-        };
-
-        dataPoint.properties = {
-          "id": parseInt(element.id),
-          "Photo": element.Photo,
-          "Tag": element.Tag,
-          "Title": element.Title,
-          "Description": element.Description
-        };
-        dataPoint.geometry.coordinates = 
-        element["Location (Longitude, Latitude)"].split(", ").reverse().map(x=>parseFloat(x));
-        data.features.push(dataPoint);
-    });
-
-    //console.log(data);
-    mapTheData(data);
-   
+  //console.log(data);
+  mapTheData(data);
 }
 
-window.addEventListener('DOMContentLoaded', loadTable);
+window.addEventListener("DOMContentLoaded", loadTable);
 
 /* 
 // FROM JSON
 var promise = $.getJSON("grafs.json");
 promise.then(function (data) { mapTheData(data);});
 */
-
